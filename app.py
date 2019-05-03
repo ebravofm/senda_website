7e5d7e2e042a58508d23dca14f73c015302778cd
ebravofm <ebravofm@gmail.com>
2019-05-03 03:05:53 -0400

    3

--- app.py
+++ app.py
@@ -1,17 +1,34 @@
 from flask import Flask, render_template
 import pandas as pd
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-    csv = pd.read_csv('files/table.csv')
+    return render_template('index.html')
+
+
+@app.route('/avance')
+def avance():
+    return render_template('avance.html')
+
+@app.route('/mapa')
+def mapa():
+    return render_template('mapa.html')
+
+
+@app.route('/tables')
+def tables():
+    csv = pd.read_csv('files/table.csv', sep=';')
     html_table = csv.to_html()
     html_table = html_table.replace(' border="1" ', ' ')
     html_table = html_table.replace('dataframe', 'table')
 
 
-    return render_template('index.html', html_table=html_table)
+    return render_template('tables.html', html_table=html_table)
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True, use_reloader=True)
