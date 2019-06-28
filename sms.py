d82a93a54087ae6e3b76070465f6f9d0c1e38681
ebravofm <ebravofm@gmail.com>
2019-06-28 10:28:08 -0400

    Added sms functionality

--- sms.py
+++ sms.py
@@ -1,6 +1,15 @@
 from flask import Blueprint
 
 sms_page = Blueprint('sms_page', __name__, template_folder='templates')
-@sms_page.route('/sms')
-def sms_func():
-    return 'Listo'
+
+@sms_page.route('/sms', methods=['POST'])
+def sms_reply():
+    """Respond to incoming calls with a simple text message."""
+    # Fetch the message
+    msg = request.form.get('Body')
+
+    # Create reply
+    resp = MessagingResponse()
+    resp.message("You said: {}".format(msg))
+
+	return str(resp)
