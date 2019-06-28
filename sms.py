ae59bb9135e0c0382009f8253286ef9c43b5a626
ebravofm <ebravofm@gmail.com>
2019-06-28 12:20:28 -0400

    Added sms functionality

--- sms.py
+++ sms.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 
 sms_page = Blueprint('sms_page', __name__, template_folder='templates')
